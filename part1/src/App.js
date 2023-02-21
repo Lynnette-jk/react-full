054a81a4741e20b2edf66fc483b40eac174e2299
Lynnette_jk <lynnettecheruto@gmail.com>
2023-02-21 16:15:52 +0300

    exercise 1.8

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const History = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad;
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positivePercentage = (good / total) * 100;
 
-  if (all === 0) {
+  if (total === 0) {
     return (
       <div>
         <h2>Give feedback</h2>
@@ -11,38 +13,35 @@ const History = ({ good, neutral, bad }) => {
     );
   }
 
-  const average = (good - bad) / all;
-  const positivePercentage = (good / all) * 100;
-
   return (
     <div>
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average score: {average.toFixed(1)}</p>
-      <p>Positive feedback: {positivePercentage.toFixed(1)}%</p>
+      <p>Total: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positivePercentage}%</p>
     </div>
   );
 };
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(good + 1)
-  }
+    setGood(good + 1);
+  };
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
+    setNeutral(neutral + 1);
+  };
 
   const handleBadClick = () => {
-    setBad(bad + 1)
-  }
+    setBad(bad + 1);
+  };
 
   return (
     <div>
@@ -50,9 +49,10 @@ const App = () => {
       <button onClick={handleGoodClick}>Good</button>
       <button onClick={handleNeutralClick}>Neutral</button>
       <button onClick={handleBadClick}>Bad</button>
-      <History good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
