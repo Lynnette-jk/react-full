dca7f1e63be16842ddd3f680a382de12f330907e
Lynnette_jk <lynnettecheruto@gmail.com>
2023-02-22 14:50:02 +0300

    exercise 1.13 version 1

--- part1/src/App.js
+++ part1/src/App.js
@@ -12,14 +12,21 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
   const handleNextAnecdoteClick = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex);
   };
-
+   const handleVoteClick = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>Votes: {votes[selected]}</p>
+      <button onClick={handleVoteClick}>Vote</button>
       <button onClick={handleNextAnecdoteClick}>Next Anecdote</button>
     </div>
   )
