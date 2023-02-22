c90da4d4c750df5ad9fe52fc25b46006d550cebe
Lynnette_jk <lynnettecheruto@gmail.com>
2023-02-22 14:35:05 +0300

    exercise 1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,72 +1,28 @@
 import { useState } from 'react';
-
-
-const StatisticLine = ({ text, value }) => (
-  <p>{text}: {value}</p>
-)
-
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad;
-  const average = (good - bad) / total || 0;
-  const positivePercentage = (good / total) * 100 || 0;
-
-  if (total === 0) {
-    return (
-      <div>
-        <h2>Statistics</h2>
-        <p>No feedback given</p>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <h2>Statistics</h2>
-      <table>
-        <tbody>
-          <StatisticLine text="Good" value={good} />
-          <StatisticLine text="Neutral" value={neutral} />
-          <StatisticLine text="Bad" value={bad} />
-          <StatisticLine text="All" value={total} />
-          <StatisticLine text="Average" value={average} />
-          <StatisticLine
-            text="Positive"
-            value={positivePercentage}
-            unit="%"
-          />
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const handleGoodClick = () => {
-    setGood(good + 1);
-  };
-
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
-  };
-
-  const handleBadClick = () => {
-    setBad(bad + 1);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const handleNextAnecdoteClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
   };
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button>
-      <button onClick={handleBadClick}>Bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleNextAnecdoteClick}>Next Anecdote</button>
     </div>
-  );
+  )
 };
 
 export default App;
