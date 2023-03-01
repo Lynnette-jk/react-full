262d825ccd624b1a74bf474484ce0d274b51476f
Lynnette_jk <lynnettecheruto@gmail.com>
2023-03-01 12:27:16 +0300

    exercise 2.2

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -1,4 +1,6 @@
 const Course = ({ course }) => {
+    const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+    
     return (
       <div>
         <h1>{course.name}</h1>
@@ -7,8 +9,10 @@ const Course = ({ course }) => {
             {part.name} {part.exercises}
           </p>
         ))}
+        <p><strong>Total of {totalExercises} exercises</strong></p>
       </div>
     );
   };
+  
 
   export default Course;
