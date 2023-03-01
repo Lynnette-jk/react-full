d7fc37602740df7ca908248b054faea75d0abd12
Lynnette_jk <lynnettecheruto@gmail.com>
2023-03-01 12:48:47 +0300

    exercise 2.4

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,8 +1,9 @@
 import Course from './components/Course'
 const App = () => {
-    const course = {
-      id: 1,
+  const courses = [
+    {
       name: 'Half Stack application development',
+      id: 1,
       parts: [
         {
           name: 'Fundamentals of React',
@@ -20,16 +21,36 @@ const App = () => {
           id: 3
         },
         {
-          name: 'Reddux',
+          name: 'Redux',
           exercises: 11,
           id: 4
         }
       ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
+  ]
+
   return (
-   
-      <Course course={course} />
-     
+    <div>
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
+    </div>
   )
 }
 
