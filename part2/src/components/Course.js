6537fc0b34ab6683b1301252785d7cd2711a7a13
Lynnette_jk <lynnettecheruto@gmail.com>
2023-03-01 13:04:01 +0300

    exercise 2.5

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -1,18 +1,42 @@
-const Course = ({ course }) => {
-    const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-    
-    return (
-      <div>
-        <h1>{course.name}</h1>
-        {course.parts.map(part => (
-          <p key={part.id}>
-            {part.name} {part.exercises}
-          </p>
-        ))}
-        <p><strong>Total of {totalExercises} exercises</strong></p>
-      </div>
-    );
-  };
-  
+import React from 'react';
 
-  export default Course;
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Total of {totalExercises} exercises</p>;
+};
+
+const Course = ({ course }) => {
+  const { name, parts } = course;
+
+  return (
+    <div>
+      <Header courseName={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default Course;
