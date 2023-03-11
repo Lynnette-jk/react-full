24e15675d470f99cb67d45b2e0d42f4841263f97
Lynnette_jk <lynnettecheruto@gmail.com>
2023-03-11 20:17:10 +0300

    receiving data

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.use(express.json())
 
 let notes = [
     {
@@ -29,13 +37,33 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
-  
+
     if (note) {
-      response.json(note)
+        response.json(note)
     } else {
-      response.status(404).end()
+        response.status(404).end()
     }
-  })
+})
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
